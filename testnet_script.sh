43331c25873147a9072c65188a98e63d7d106215
dk808zelnode <47246006+dk808zelnode@users.noreply.github.com>
2019-02-02 04:21:28 -0800

    Update testnet_script.sh

--- testnet_script.sh
+++ testnet_script.sh
@@ -4,7 +4,7 @@ COIN_NAME='ZELCASH' #no spaces
 
 #wallet information
 WALLET_DOWNLOAD='https://www.dropbox.com/s/raw/y4wxr5ok2a7ug65/zelnode-testnetv2.zip'
-WALLET_TAR_FILE='zelnode-testnetv2-linux.zip'
+WALLET_TAR_FILE='zelnode-testnetv2.zip'
 ZIPTAR='unzip' #can be either unzip or tar -xfzg
 EXTRACT_DIR='' #not always necessary, can be blank if zip/tar file has no subdirectories
 CONFIG_FILE='zelcash.conf'
