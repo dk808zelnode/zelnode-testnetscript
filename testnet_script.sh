b1100aef466d0464e6c8335330430f6a17c155eb
dk808zelnode <47246006+dk808zelnode@users.noreply.github.com>
2019-02-11 02:27:03 -0800

    Update testnet_script.sh

--- testnet_script.sh
+++ testnet_script.sh
@@ -40,7 +40,6 @@ sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get dist-upgrade -y
 sudo apt-get install git -y
 sudo apt-get install curl -y
 sudo apt-get install nano -y
